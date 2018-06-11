8494488a51bf4d090a60cb0ae753c74791275f83
Matt McMahon <matt@broken-toys.com>
2018-06-11 17:09:01 -0400

    New function: equal

---
+++ source/equal.js
@@ -0,0 +1,6 @@
+'use strict'
+
+const equal = a => b => a === b
+equal.signature = 'equal :: a => b => boolean'
+
+module.exports = { equal }
---
+++ source/equal.test.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const test = require('tape')
+const { equal } = require('./equal')
+
+test('equal function', t => {
+  t.equal(typeof equal, 'function', 'equal should be a function')
+  t.equal(
+    typeof equal.signature,
+    'string',
+    'equal.signature should be a string'
+  )
+
+  const equalsA = equal('a')
+
+  {
+    const actual = typeof equalsA
+    const expected = 'function'
+    const message = 'equal(a) should create a function'
+    t.equal(actual, expected, message)
+  }
+
+  {
+    const actual = equalsA('a')
+    const expected = true
+    const message = `equals('a')('a') === true`
+    t.equal(actual, expected, message)
+  }
+
+  {
+    const actual = equalsA('b')
+    const expected = false
+    const message = `equals('a')('b') === false`
+    t.equal(actual, expected, message)
+  }
+
+  t.end()
+})
