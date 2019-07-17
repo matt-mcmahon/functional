9fd427972ba575093fe32ace4ba671438119efef
Matt McMahon <contact@matt-mcmahon.com>
2019-07-17 15:04:30 -0400

    New Module: not

---
+++ source/not/index.d.ts
@@ -0,0 +1,15 @@
+/**
+ * ```
+ * not
+ * ```
+ * -----------------------------------------------------------------------------
+ * Takes a _value_ and returns it's [logical not][1].
+ *
+ * ```
+ * not('foo') <=> !'foo' <=> false
+ * ```
+ *
+ * [1]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_Operators#Logical_NOT_(!)
+ *
+ */
+export declare function not(a: any): boolean
---
+++ source/not/not.js
@@ -0,0 +1,7 @@
+import { sign } from "@mwm/sign"
+
+export const signatures = ["not :: a => boolean"]
+
+export const implementation = value => !value
+
+export const not = sign(signatures, implementation)
