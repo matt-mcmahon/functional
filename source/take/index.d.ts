8d5e0be9d6806c3674283638f96a38a9290fe794
Matt McMahon <contact@matt-mcmahon.com>
2019-07-17 15:13:27 -0400

    New Module: take

---
+++ source/take/index.d.ts
@@ -0,0 +1,10 @@
+/**
+ * ```
+ * take :: (n, ...as) => as
+ * ```
+ * -----------------------------------------------------------------------------
+ *
+ * Returns the first _n_ elements of the gathered list.
+ *
+ */
+export declare function take(n: Number, ...list: any[]): any[]
