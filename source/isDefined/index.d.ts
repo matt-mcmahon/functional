0536972c328a5b6deb9c8d3a8efe3cdbfc9ef415
Matt McMahon <contact@matt-mcmahon.com>
2019-07-17 15:01:19 -0400

    New Module: isDefined

---
+++ source/isDefined/index.d.ts
@@ -0,0 +1,10 @@
+/**
+ * ```
+ * isDefined :: a => boolean
+ * ```
+ * -----------------------------------------------------------------------------
+ *
+ * Returns true unless a is `NaN`, `undefined`, or `null`
+ *
+ */
+export declare function isDefined(value: any): boolean
---
+++ source/isDefined/isDefined.test.js
@@ -0,0 +1,30 @@
+import { describe } from "@mwm/describe"
+import { isDefined, implementation, signatures } from "./isDefined"
+
+describe(
+  {
+    path: "source/isDefined",
+    public: [isDefined],
+    private: [implementation, signatures],
+  },
+  async ({ assert, inspect }) => {
+    const data = [
+      [null, false],
+      [undefined, false],
+      [NaN, false],
+      ["", true],
+      [[], true],
+      [false, true],
+      [{}, true],
+      [{ length: 0 }, true],
+    ]
+
+    const test = ([value, expected]) => {
+      const actual = isDefined(value)
+      const given = inspect`isDefined(${value})`
+      assert({ given, actual, expected })
+    }
+
+    data.forEach(test)
+  }
+)
