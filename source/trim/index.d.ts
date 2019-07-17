5beabc9e7c4a75c815d8799dd968652807429849
Matt McMahon <contact@matt-mcmahon.com>
2019-07-17 15:06:19 -0400

    New Module: trim

---
+++ source/trim/index.d.ts
@@ -0,0 +1,15 @@
+/**
+ * ```
+ * trim :: s => s
+ * ```
+ * -----------------------------------------------------------------------------
+ *
+ * Takes a string and trims any whitespace characters that appear at the
+ * beginning or end, as in:
+ *
+ * ```
+ * trim(" s ") <=> " s ".trim() <=> "s"
+ * ```
+ *
+ */
+export declare function trim(string: string): string
