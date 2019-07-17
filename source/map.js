1f5005f530d53dceddc44fdfd7f3f108cdf4ac71
Matt McMahon <contact@matt-mcmahon.com>
2019-07-17 14:22:11 -0400

    Edit: map
    - Migrated to new project structure.
    - Updated tests, types, and exports.

--- source/map.js
+++
@@ -1,7 +0,0 @@
-import { sign } from "./util"
-
-const map = fun => values => values.map(fun)
-
-sign("map :: (a -> b) -> as -> bs")(map)
-
-export { map, map as default }
---
+++ source/map/map.js
@@ -0,0 +1,10 @@
+import { sign } from "@mwm/sign"
+
+export const signatures = [
+  { "map :: (a -> b) -> as -> bs": 1 },
+  { "map ::             as -> bs": 1 },
+]
+
+export const implementation = mapAB => as => as.map(mapAB)
+
+export const map = sign(signatures, implementation)
