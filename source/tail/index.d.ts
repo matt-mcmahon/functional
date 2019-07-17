26cc6d150f8790993e1220dbf85fb1d16346aa64
Matt McMahon <contact@matt-mcmahon.com>
2019-07-17 14:34:15 -0400

    Edit: tail
    - Migrated to new project structure.
    - Updated tests, types, and exports.

---
+++ source/tail/index.d.ts
@@ -0,0 +1,10 @@
+/**
+ * ```
+ * tail :: as => as
+ * ```
+ * -----------------------------------------------------------------------------
+ *
+ * Returns the portion of an array not including the first element.
+ *
+ */
+export declare function tail(as: any[]): any[]
