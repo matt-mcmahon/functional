44378f40c919be556176498a0b5b21712003a340
Matt McMahon <contact@matt-mcmahon.com>
2019-07-18 10:18:39 -0400

    Edit: isNil
    - Updated type signatures.

--- source/isNil/index.d.ts
+++ source/isNil/index.d.ts
@@ -4,12 +4,13 @@ import { Predicate } from "../types"
  * isNil :: a => boolean
  * ```
  * -----------------------------------------------------------------------------
- * __Predicate__ that returns `true` if it's argument is `null` or
- * `undefined`, `false` otherwise. For example
+ * _Predicate_ that returns `true` if __a__ is `null` or `undefined`,
+ * `false` otherwise. For example
  *
  * ```
- * isNil(null)      //> true
- * isNil(undefined) //> true
- * isNil(0)         //> false
+ * isNil(null)      <=> true
+ * isNil(undefined) <=> true
+ * isNil(0)         <=> false
  * ```
- */ export declare const isNil: Predicate<any>
+ */
+export declare function isNil<A>(a: A): boolean
