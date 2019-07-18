af1300ec5175aad3c50d624ac01722fbaaaa3e5e
Matt McMahon <contact@matt-mcmahon.com>
2019-07-18 10:26:59 -0400

    Edit: pipeV
    - Updated type signatures.

--- source/pipeV/index.d.ts
+++ source/pipeV/index.d.ts
@@ -2,7 +2,7 @@ import { Unary } from "../types"
 
 /**
  * ```
- * pipeV :: a => ((a => b), (b => c), ..., (y => z)) => z
+ * pipeV :: (...as) => ((a⁰ => a¹), (a¹ => a²), ..., (aⁿ=> b)) =>  b
  * ```
  *
  * _PipeV_ is a _Variadic_ _Combinator_, that takes a __value__ and one or more _Unary_ __functions__. It _composes_ the functions in left-to-right order — evaluating the first function and applying it's result to the second, it's result to third, etc. — and returns the result of evaluating the final function. E.g.:
