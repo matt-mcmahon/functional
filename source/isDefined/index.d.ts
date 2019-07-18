e493545702cfc3ca2b4308c4aa22e285aea6ae7b
Matt McMahon <contact@matt-mcmahon.com>
2019-07-18 10:12:21 -0400

    Edit: isDefined
    - Updated type signatures.

--- source/isDefined/index.d.ts
+++ source/isDefined/index.d.ts
@@ -4,7 +4,8 @@
  * ```
  * -----------------------------------------------------------------------------
  *
- * Returns true unless a is `NaN`, `undefined`, or `null`
+ * Returns `false` if __a__ is `NaN`, `undefined`, or `null`, otherwise returns
+ * `true`.
  *
  */
 export declare function isDefined(value: any): boolean
