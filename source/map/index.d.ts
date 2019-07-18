1715297ad034f64fc45821c4cdcd55cd2e8d3fef
Matt McMahon <contact@matt-mcmahon.com>
2019-07-18 10:22:47 -0400

    Edit: map
    - Updated signatures

--- source/map/index.d.ts
+++ source/map/index.d.ts
@@ -2,14 +2,14 @@ import { Unary, Mappable } from "../types"
 
 /**
  * ```
- * map :: (a -> b) -> as -> bs
+ * map :: (a => b) => as => bs
  * ```
  * -----------------------------------------------------------------------------
  *
  * Takes a __Unary__ function and a __Mappable__, applies the function to
  * each of the mappable values. Returns a mappable of the same type.
  */
-export declare function map<T, U>(
-  unary: Unary<T, U>,
-  iterable: Mappable<T>
-): Mappable<T>
+export declare function map<A, B>(
+  mapAB: Unary<A, B>,
+  as: Mappable<A>
+): Mappable<B>
--- source/map/map.js
+++ source/map/map.js
@@ -1,8 +1,8 @@
 import { sign } from "@mwm/sign"
 
 export const signatures = [
-  { "map :: (a -> b) -> as -> bs": 1 },
-  { "map ::             as -> bs": 1 },
+  { "map :: (a => b) => as => bs": 1 },
+  { "map ::             as => bs": 1 },
 ]
 
 export const implementation = mapAB => as => as.map(mapAB)
