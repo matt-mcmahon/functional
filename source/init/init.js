33302b82f253f8f1c041281bfda79bcaa054cb4f
Matt McMahon <contact@matt-mcmahon.com>
2019-07-18 10:05:43 -0400

    Edit: init
    - Updated type signatures.

--- source/init/init.js
+++ source/init/init.js
@@ -1,6 +1,6 @@
 import { sign } from "@mwm/sign"
 
-export const signatures = ["init :: as -> as"]
+export const signatures = ["init :: as => as"]
 
 export const implementation = as => as.slice(0, as.length - 1)
 
