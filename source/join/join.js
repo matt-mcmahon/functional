0a955b4a0b205ad111e165d52031582a632584d9
Matt McMahon <contact@matt-mcmahon.com>
2019-07-18 10:19:16 -0400

    Edit: join
    - Updated type signatures

--- source/join/join.js
+++ source/join/join.js
@@ -1,10 +1,10 @@
 import { sign } from "@mwm/sign"
 
 export const signatures = [
-  { "join->withCharacter :: c => as => s": 1 },
+  { "join->withCharacter :: s => as => s": 1 },
   { "join->list          ::      as => s": 1 },
 ]
 
-export const implementation = withCharacter => list => list.join(withCharacter)
+export const implementation = s => as => as.join(s)
 
 export const join = sign(signatures, implementation)
