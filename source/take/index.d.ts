211f2693c67879214479c0fd5081c63c98292b60
Matt McMahon <contact@matt-mcmahon.com>
2019-07-18 10:36:40 -0400

    Edit: take
    - Updated type signature.
    - Clarified that it accepts a signle array as it's argument.

--- source/take/index.d.ts
+++ source/take/index.d.ts
@@ -1,10 +1,10 @@
 /**
  * ```
- * take :: (n, ...as) => as
+ * take :: (n, as) => as
  * ```
  * -----------------------------------------------------------------------------
  *
- * Returns the first _n_ elements of the gathered list.
+ * Returns the first _n_ elements of the given list.
  *
  */
-export declare function take(n: Number, ...list: any[]): any[]
+export declare function take<A>(n: Number, list: A[]): A[]
