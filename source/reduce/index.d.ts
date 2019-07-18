3f4191a69b4c33ae00ca9c5003507aeb152bdd2f
Matt McMahon <contact@matt-mcmahon.com>
2019-07-18 10:32:47 -0400

    Edit: reduce
    - Updated type signature.

--- source/reduce/index.d.ts
+++ source/reduce/index.d.ts
@@ -2,13 +2,13 @@ import { Reducer, Iterable } from "../types"
 
 /**
  * ```
- * reduce ::
+ * reduce :: ((a, b) => a) => a => [b⁰, b¹, b², ..., bⁿ] => a
  * ```
  * -----------------------------------------------------------------------------
  *
- * __reduce__ is a _Unary_ _Combinator_, that reduces an _Iterrable_, in
+ * __reduce__ is a _Unary_ _Combinator_, that reduces an _Iterable_, in
  * first-to-last order, to a single value. It does this by passing an initial
- * value, the _accumulator_ and the current value of the _Iterrable_, to a
+ * value, the _accumulator_ and the current value of the _Iterable_, to a
  * _reducer_ function. The return value for that call is used as the accumulator
  * for the next iteration.
  *
