fa0ddcf3ca8a9f36f5db4014d5f09e43fc73180f
Matt McMahon <contact@matt-mcmahon.com>
2019-07-18 10:24:38 -0400

    Edit: not
    - Updated type signatures.

--- source/not/index.d.ts
+++ source/not/index.d.ts
@@ -1,9 +1,10 @@
 /**
  * ```
- * not
+ * not :: a => boolean
  * ```
  * -----------------------------------------------------------------------------
- * Takes a _value_ and returns it's [logical not][1].
+ *
+ * Takes a _value_, __a__, and returns it's [logical not][1].
  *
  * ```
  * not('foo') <=> !'foo' <=> false
@@ -12,4 +13,4 @@
  * [1]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_Operators#Logical_NOT_(!)
  *
  */
-export declare function not(a: any): boolean
+export declare function not<A>(a: A): boolean
