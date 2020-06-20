5e35effd5eaa26b0c54691234e1d7a8e534228c6
Matt McMahon <contact@matt-mcmahon.com>
2020-06-20 10:20:19 -0400

    Converted clone.ts

--- source/clone/clone.test.ts
+++ source/clone/clone.test.ts
@@ -1,250 +1,97 @@
-import { describe } from "@mwm/describe";
-import { clone, implementation, signatures } from "./clone.ts";
+import { describe } from "../../lib/describe.ts";
+import { clone } from "./clone.ts";
 
-describe(
-  {
-    path: "source/clone",
-    public: [clone],
-    private: [signatures, implementation],
-  },
-  async ({ assert, inspect }) => {
-    {
-      const original = { foo: "foo", bar: "bar" };
-      const cloned = clone(original);
+describe("functional/clone.ts", async ({ assert, inspect }) => {
+  const original = { foo: "foo", bar: "bar" };
+  const copy = clone(original);
+  clone(original).bar = "BAZ";
+  assert({
+    actual: original === copy,
+    expected: false,
+    given: inspect`origin === copy`,
+  });
+  assert(
+    { actual: copy === original, expected: false, given: `original === copy` },
+  );
+});
 
-      {
-        const given = inspect`a flat-object`;
-        const should = inspect`be a new object instance`;
-        const actual = cloned !== original;
-        assert({ given, should, actual });
-      }
+describe("functional/clone.ts->shallow", async ({ assert, inspect }) => {
+  const original = { foo: "foo", bar: "bar" };
+  const copy = clone(original);
+  copy.bar = "BAZ";
+  assert({
+    actual: original === copy,
+    expected: false,
+    given: inspect`origin === copy`,
+  });
+  assert({ actual: original, expected: { foo: "foo", bar: "bar" } });
+  assert({ actual: copy, expected: { foo: "foo", bar: "BAZ" } });
+});
 
-      {
-        const given = `a cloned object`;
-        const should = `have the same properties and values as the original`;
-        const actual = cloned;
-        const expected = original;
-        assert({ given, should, actual, expected });
-      }
-    }
+describe("functional/clone.ts->deep", async ({ assert, inspect }) => {
+  const original = { foo: "foo", bar: { baz: "baz" } };
+  const copy = clone(original);
+  copy.bar.baz = "BAZ";
+  assert({
+    actual: original === copy,
+    expected: false,
+    given: inspect`origin === copy`,
+  });
+  assert({ actual: original, expected: { foo: "foo", bar: { baz: "baz" } } });
+  assert({ actual: copy, expected: { foo: "foo", bar: { baz: "BAZ" } } });
+});
 
-    {
-      const original = {
-        foo: "foo",
-        bar: {
-          baz: "baz",
-        },
-      };
-      const cloned = clone(original);
-      const given = inspect`a nested object`;
+describe("functional/clone.ts->undefined", async ({ assert, inspect }) => {
+  const value = undefined;
+  const original: { foo: string; bar?: string } = { foo: "foo" };
+  const copy = clone(original);
+  copy.bar = "bar";
+  assert({ actual: clone(value), expected: value });
+  assert({ actual: original, expected: { foo: "foo" } });
+  assert({ actual: copy, expected: { foo: "foo", bar: "bar" } });
+});
 
-      {
-        const should = inspect`have equivalent nested properties`;
-        const actual = cloned;
-        const expected = original;
-        assert({ given, should, actual, expected });
-      }
+describe("functional/clone.ts->null", async ({ assert, inspect }) => {
+  const value = null;
+  const original: { foo: string; bar: string | null } = {
+    foo: "foo",
+    bar: null,
+  };
+  const copy = clone(original);
+  copy.bar = "bar";
+  assert({ actual: clone(value), expected: value });
+  assert({ actual: original, expected: { foo: "foo", bar: null } });
+  assert({ actual: copy, expected: { foo: "foo", bar: "bar" } });
+});
 
-      {
-        const should = inspect`be a new instance`;
-        const actual = cloned.bar !== original.bar;
-        assert({ given, should, actual });
-      }
-    }
+describe("functional/clone.ts->date", async ({ assert, inspect }) => {
+  const original = new Date(Date.now());
+  const copy = clone(original);
+  assert({
+    actual: original === copy,
+    expected: false,
+    given: inspect`origin === copy`,
+  });
+  assert({ actual: copy, expected: original });
+  assert({
+    given: inspect`${copy} instanceof Date?`,
+    actual: copy instanceof Date,
+    expected: true,
+  });
+});
 
-    {
-      const given = inspect`a flat array`;
-      const expected = ["foo", "bar", "bax"];
-      const actual = clone(expected);
-      assert({ given, actual, expected });
-      assert({
-        given,
-        should: `be a new array instance`,
-        actual: actual !== expected,
-      });
-    }
-
-    {
-      const expected = [
-        ["foo", { bar: "bar" }],
-        ["baz", "qux"],
-      ];
-      const actual = clone(expected);
-      const given = inspect`a nested, mixed array`;
-
-      assert({
-        given,
-        actual,
-        expected,
-      });
-
-      assert({
-        given,
-        should: inspect`make new instances (0)`,
-        actual: actual[0] !== expected[0],
-      });
-
-      assert({
-        given,
-        should: inspect`make new instances (0,1)`,
-        actual: actual[0][1] !== expected[0][1],
-      });
-
-      assert({
-        given,
-        should: inspect`make new instances (1)`,
-        actual: actual[1] !== expected[1],
-      });
-    }
-
-    {
-      const expected = {
-        foo: ["foo", { baz: "baz" }],
-        bar: [null],
-      };
-      const actual = clone(expected);
-      const given = `a nested, mixed object`;
-
-      assert({ given, actual, expected });
-
-      assert({
-        given,
-        should: inspect`create new ${"foo"} array instances`,
-        actual: actual.foo !== expected.foo,
-      });
-
-      assert({
-        actual,
-        should: inspect`create new ${"bar"} array instances`,
-        actual: actual.bar !== expected.bar,
-      });
-
-      assert({
-        given,
-        should: inspect`create new ${{ baz: "baz" }} objects`,
-        actual: actual.foo[1] !== expected.foo[1],
-      });
-
-      assert({
-        given,
-        should: inspect`copy ${null} values`,
-        actual: actual.bar[0],
-        expected: null,
-      });
-    }
-
-    {
-      // test('clone with circular references', assert => {
-      const expected = { bar: "bar" };
-      expected.bazInArray = [expected];
-      expected.bazAsProperty = expected;
-      const actual = clone(expected);
-
-      assert.doesNotThrow(() => {
-        // Cannot use tape's deep equal. It is not circular reference safe.
-        assert({ actual, expected });
-      }, `should clone circular references`);
-
-      const given = inspect`circular references`;
-      assert({
-        given,
-        should: inspect`clone not copy bazInArray`,
-        actual: actual.bazInArray !== expected.bazInArray,
-      });
-
-      assert({
-        given,
-        should: inspect`clone not copy bazAsProperty`,
-        actual: actual.bazAsProperty !== expected.bazAsProperty,
-      });
-
-      assert({
-        given: inspect`clone's bazInArray[0]`,
-        should: inspect`be a new object`,
-        actual: actual.bazInArray[0] !== expected.bazInArray[0],
-      });
-
-      assert({
-        given: inspect`clone's bazInArray[0]`,
-        should: inspect`should refrence the clone`,
-        actual: actual.bazInArray[0] === actual,
-      });
-
-      assert({
-        given: inspect`clone's bazAsProperty`,
-        should: inspect`should refrence the clone`,
-        actual: actual.bazAsProperty === actual,
-      });
-    }
-
-    {
-      const expected = new Date();
-      const actual = clone(expected);
-      const given = inspect`a cloned date, ${expected}`;
-
-      assert({
-        given,
-        should: inspect`be equivalent`,
-        actual: actual.toString(),
-        expected: expected.toString(),
-      });
-
-      assert({
-        given,
-        should: inspect`a new instance`,
-        actual: expected !== actual,
-      });
-    }
-
-    {
-      const f = () => true;
-      f.baz = "baz";
-      const g = clone(f, null);
-      const given = inspect`a clone of function ${f}`;
-
-      assert.doesNotThrow(g, `given ${given}; should be invokable`);
-
-      {
-        const should = inspect`have the property, ${{ baz: "baz" }}`;
-        const actual = g.baz;
-        const expected = f.baz;
-        assert({ given, should, actual, expected });
-      }
-    }
-  },
-);
-
-/**
- * Adapted from:
- *
- *   https://github.com/thebearingedge/deep-clone/blob/master/test/index.js
- *
- *
- *
- * Original License:
- * ============================================================================
- *
- * The MIT License (MIT)
- *
- * Copyright (c) 2015 Tim Davis
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to
- * deal in the Software without restriction, including without limitation the
- * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
- * sell copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
- *
- * ============================================================================
- */
+describe("functional/clone.ts->array", async ({ assert, inspect }) => {
+  const original = [1, 2, 3];
+  const copy = clone(original);
+  assert({
+    actual: original === copy,
+    expected: false,
+    given: inspect`origin === copy`,
+  });
+  assert({ actual: copy, expected: original });
+  assert({
+    actual: Array.isArray(copy),
+    expected: true,
+    given: `copy of array is array`,
+  });
+});
--- source/clone/clone.ts
+++ source/clone/clone.ts
@@ -1,90 +1,43 @@
-import { sign } from "@mwm/sign";
+import { assoc } from "./assoc.ts";
+import { ifElse } from "./if-else.ts";
+import { identity } from "./identity.ts";
+import { isObject, isNil } from "./is.ts";
 
-export const signatures = [{ "clone :: (a, this?) => a": 2 }];
+function cloneObject<A extends object>(a: A, map = new WeakMap()): A {
+  if (map.has(a)) return map.get(a);
+  const c = Object.entries(a).reduce((clone, [key, value]) => {
+    return Object.assign(clone, { [key]: cloneUnknown(value, map) });
+  }, {} as A);
+  map.set(a, c);
+  return c;
+}
 
-export const implementation = (value, context) =>
-  recursiveClone(new WeakMap(), value, context);
+function cloneUnknown<A>(a: A, map = new WeakMap()): A {
+  if (isNil(a)) {
+    return a;
+  }
+  if (a instanceof Date) {
+    return new Date(a.valueOf()) as unknown as A;
+  }
+  if (Array.isArray(a)) {
+    return a.map((v) => cloneUnknown(v, map)) as unknown as A;
+  }
+  if (typeof a === "object" || typeof a === "function") {
+    return cloneObject(a as unknown as object, map) as unknown as A;
+  }
+  return a;
+}
 
 /**
  * ```
- * clone :: (a, this?) => a
+ * clone :: a => a
  * ```
- * -----------------------------------------------------------------------------
- *
- * Returns a deep-clone of it's argument, `a`.
- *
- * __Warning__: _clone_ cannot create true copies of a _Method_ or any function
- * with a dynamic `this`. If you need to clone a _Method_, supply a `context`
- * parameter to fix the value for `this` inside the function, or it may not
- * behave as expected.
+ * @export
+ * @template A
+ * @param {A} a
+ * @returns {A}
  */
-export const clone = sign(signatures, implementation);
-
-const recursiveClone = (map, value, context) => {
-  const existingClone = map.get(value);
-  if (existingClone) return existingClone;
-  if (typeof value === "function") return cloneFunction(map, value, context);
-  if (Array.isArray(value)) return cloneArray(map, value);
-  if (value instanceof Date) return cloneDate(value);
-  if (value instanceof Object) return cloneObject(map, value);
-  return value;
-};
-
-const cloneFunction = (map, fun, context) =>
-  cloneObject(map, fun, fun.bind(context));
-
-const cloneArray = (map, arr) => {
-  const copy = [];
-  map.set(arr, copy);
-  arr.reduce((copy, value) => {
-    copy.push(recursiveClone(map, value));
-    return copy;
-  }, copy);
-  return copy;
-};
-
-const cloneDate = (date) => new Date(date.valueOf());
-
-const cloneObject = (map, obj, copy = {}) => {
-  map.set(obj, copy);
-  const keys = Object.keys(obj);
-  keys.reduce((copy, key) => {
-    copy[key] = recursiveClone(map, obj[key]);
-    return copy;
-  }, copy);
-  return copy;
-};
-
-/**
- * Adapted from:
- *
- *   https://github.com/thebearingedge/deep-clone/blob/master/src/index.js
- *
- *
- * Original License:
- * ============================================================================
- *
- * The MIT License (MIT)
- *
- * Copyright (c) 2015 Tim Davis
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to
- * deal in the Software without restriction, including without limitation the
- * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
- * sell copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
- *
- * ============================================================================
- */
+export function clone<A>(a: A): A {
+  const map = new WeakMap();
+  return cloneUnknown(a, map);
+}
