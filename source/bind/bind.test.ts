63d2c428e67aafa2733fae6ee30c2248342ed496
Matt McMahon <contact@matt-mcmahon.com>
2020-06-20 10:19:51 -0400

    Convert bind.ts

--- source/bind/bind.test.ts
+++ source/bind/bind.test.ts
@@ -1,78 +1,70 @@
-import { describe } from "@mwm/describe";
-import { bind, implementation, signatures } from "./bind.ts";
+import { describe } from "../../lib/describe.ts";
+import { bind } from "./bind.ts";
 
-describe(
+describe("source/bind.ts", async ({ assert, inspect }) => {
+  type Foo = {
+    method(foo: string): string;
+    foo?: string;
+  };
+
+  const methodSource: Foo = {
+    foo: "original",
+    method(value: string) {
+      this.foo = value;
+      return "method called";
+    },
+  };
+
+  const bind1 = bind(methodSource.method);
+
   {
-    path: "source/bind",
-    public: [bind],
-    private: [signatures, implementation],
-  },
-  async ({ assert, inspect }) => {
-    const objectWith = {
-      method(value) {
-        this.foo = value;
-        return "method called";
-      },
+    const actual = typeof bind1;
+    const expected = "function";
+    const given = inspect`${bind1}`;
+    const should = inspect`be a ${expected} (1 applied arguments)`;
+    assert({ given, should, actual, expected });
+  }
+
+  const bindTarget = {
+    bar: "bar",
+  };
+
+  const bind2 = bind1(bindTarget);
+
+  {
+    const actual = typeof bind2;
+    const expected = "function";
+    const given = inspect`${bind2}`;
+    const should = inspect`be a ${expected} (2 applied arguments)`;
+    assert({ given, should, actual, expected });
+  }
+
+  const bind3 = bind2("foo");
+
+  {
+    const actual = bind3;
+    const expected = "method called";
+    const given = inspect`three applied arguments`;
+    assert({ given, actual, expected });
+  }
+
+  {
+    const given = inspect`the methodSource object`;
+    const should = inspect`have ${{
+      foo: "original",
+    }}`;
+    const { foo: actual } = methodSource;
+    const expected = "original";
+    assert({ given, should, actual, expected });
+  }
+
+  {
+    const actual = { misdirection: bind2 };
+    const expected = {
+      misdirection: bind2,
     };
-
-    const bind0 = bind;
-    const bind1 = bind0(objectWith.method);
-
-    {
-      const actual = typeof bind1;
-      const expected = "function";
-      const given = inspect`${bind1}`;
-      const should = inspect`be a ${expected} (1 applied arguments)`;
-      assert({ given, should, actual, expected });
-    }
-
-    const boundTarget = {
-      bar: "bar",
-    };
-
-    const bind2 = bind1(boundTarget);
-
-    {
-      const actual = typeof bind2;
-      const expected = "function";
-      const given = inspect`${bind2}`;
-      const should = inspect`be a ${expected} (2 applied arguments)`;
-      assert({ given, should, actual, expected });
-    }
-
-    const bind3 = bind2("foo");
-
-    {
-      const actual = bind3;
-      const expected = "method called";
-      const given = inspect`three applied arguments`;
-      const should = inspect`be the value ${expected}`;
-      assert({ given, should, actual, expected });
-    }
-
-    {
-      const given = inspect`the original ${"boundTarget"} object`;
-      const should = inspect`have ${{
-        foo: "BAZ",
-      }}`;
-      const actual = boundTarget;
-      const expected = {
-        foo: "foo",
-        bar: "bar",
-      };
-      assert({ given, should, actual, expected });
-    }
-
-    {
-      const actual = { misdirection: bind2 };
-      const expected = {
-        misdirection: bind2,
-      };
-      const given = inspect`${actual.misdirection("BAZ")} on ${"actual"}`;
-      const should = inspect`not have ${{
-        foo: "BAZ",
-      }}`;
-      assert({ given, should, actual, expected });
-    }
-  },
-);
+    const given = inspect`${actual.misdirection("BAZ")} on misdirection`;
+    const should = inspect`not have ${{ foo: "BAZ" }}`;
+    assert({ given, should, actual, expected });
+  }
+});
--- source/bind/bind.ts
+++ source/bind/bind.ts
@@ -1,12 +1,3 @@
-import { sign } from "@mwm/sign";
-
-export const signatures = [
-  { "bind->method :: (...as #> b) => o => ...as => b": 1 },
-  { "bind->this   ::                 o => ...as => b": 1 },
-  { "bound        ::                      ...as => b": Infinity },
-];
-export const implementation = (m) => (t) => (...as) => m.apply(t, as);
-
 /**
  * ```
  * bind :: (...as => b) => o => ...as => b
@@ -17,4 +8,4 @@ export const implementation = (m) => (t) => (...as) => m.apply(t, as);
  * @param method a function that depends on a dynamic `this` context
  * @param object the method's context
  */
-export const bind = sign(signatures, implementation);
+export const bind = <F extends Function>(m: F) => <B>(b: B): F => m.bind(b);
