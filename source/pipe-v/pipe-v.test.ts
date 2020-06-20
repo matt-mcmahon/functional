b7c048626561d88c94f14cf46b6974266bd92710
Matt McMahon <contact@matt-mcmahon.com>
2020-06-20 16:22:05 -0400

    Non-typesafe implementation of pipeV

--- source/pipe-v/pipe-v.test.ts
+++ source/pipe-v/pipe-v.test.ts
@@ -1,38 +1,29 @@
-import { describe } from "@mwm/describe";
-import { pipeV, implementation, signatures } from "./pipe-v.ts";
+import { describe } from "../../lib/describe.ts";
+import { pipeV } from "./pipe-v.ts";
 
-describe(
+describe("source/pipeV", async ({ assert, inspect }) => {
+  const f = (x: number) => x + 1;
+  const g = (x: number) => 2 * x;
   {
-    path: "source/pipeV",
-    public: [pipeV],
-    private: [implementation, signatures],
-  },
-  async ({ assert, inspect }) => {
-    const f = (x) => x + 1;
-    const g = (x) => 2 * x;
-    {
-      const given = inspect`pipeV(${3}, ${f}, ${g})`;
-      const should = inspect
-        `have order of operation "${2} × (x + ${1}) = ${8}"`;
-      const actual = pipeV(3)(f, g);
-      const expected = 8;
-      assert({ given, should, actual, expected });
-    }
-    {
-      const given = inspect`pipeV(${3})(${f}, ${g})`;
-      const should = inspect
-        `have order of operation "${2} × (x + ${1}) = ${8}"`;
-      const actual = pipeV(3)(f, g);
-      const expected = 8;
-      assert({ given, should, actual, expected });
-    }
-    {
-      const given = inspect`pipeV(${3})(${f}, ${g})`;
-      const should = inspect
-        `not have order of operation "(${2} × x) + ${1}) = ${7}"`;
-      const actual = pipeV(3)(f, g) !== 7;
-      const expected = true;
-      assert({ given, should, actual, expected });
-    }
-  },
-);
+    const given = inspect`pipeV(${3}, ${f}, ${g})`;
+    const should = inspect`have order of operation "${2} × (x + ${1}) = ${8}"`;
+    const actual = pipeV(3)(f, g);
+    const expected = 8;
+    assert({ given, should, actual, expected });
+  }
+  {
+    const given = inspect`pipeV(${3})(${f}, ${g})`;
+    const should = inspect`have order of operation "${2} × (x + ${1}) = ${8}"`;
+    const actual = pipeV(3)(f, g);
+    const expected = 8;
+    assert({ given, should, actual, expected });
+  }
+  {
+    const given = inspect`pipeV(${3})(${f}, ${g})`;
+    const should = inspect
+      `not have order of operation "(${2} × x) + ${1}) = ${7}"`;
+    const actual = pipeV(3)(f, g) !== 7;
+    const expected = true;
+    assert({ given, should, actual, expected });
+  }
+});
