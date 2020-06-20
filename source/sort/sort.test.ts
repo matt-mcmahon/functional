ac1d3e7fe3995176d03713f17c110ef3d072fb12
Matt McMahon <contact@matt-mcmahon.com>
2020-06-20 17:19:10 -0400

    Converted: sort.ts

--- source/sort/sort.test.ts
+++ source/sort/sort.test.ts
@@ -1,32 +1,25 @@
-import { describe } from "@mwm/describe";
-import { sort, implementation, signatures } from "./sort.ts";
+import { describe } from "../../lib/describe.ts";
+import { sort } from "./sort.ts";
 
-describe(
+describe("source/sort", async ({ assert, inspect }) => {
+  const as = [3, 5, 9, 22, 1, 0];
+  const bs = [0, 1, 3, 5, 9, 22];
+  const fun = sort((a: number, b: number) => a - b);
+
   {
-    path: "source/sort",
-    public: [sort],
-    private: [implementation, signatures],
-  },
-  async ({ assert, inspect }) => {
-    const as = [3, 5, 9, 22, 1, 0];
-    const bs = [0, 1, 3, 5, 9, 22];
-    const fun = sort((a, b) => a - b);
+    const actual = fun(as);
+    const expected = bs;
+    const given = inspect`sorting numbers`;
+    const should = inspect`${expected}`;
+    assert({ actual, expected, given, should });
+  }
 
-    {
-      const actual = fun(as);
-      const expected = bs;
-      const given = inspect`sorting numbers`;
-      const should = inspect`${expected}`;
-      assert({ actual, expected, given, should });
-    }
-
-    {
-      const value = [];
-      const expected = [];
-      const actual = fun(value);
-      const given = inspect`fun(${value})`;
-      const should = inspect`work with an empty sortable.`;
-      assert({ expected, actual, given, should });
-    }
-  },
-);
+  {
+    const value: number[] = [];
+    const expected: number[] = [];
+    const actual = fun(value);
+    const given = inspect`fun(${value})`;
+    const should = inspect`work with an empty sortable.`;
+    assert({ expected, actual, given, should });
+  }
+});
--- source/sort/sort.ts
+++ source/sort/sort.ts
@@ -1,12 +1,3 @@
-import { sign } from "@mwm/sign";
-
-export const signatures = [
-  "sort->sortingFunction :: ((a, a) => n) => as => as",
-  "sort->list            ::                  as => as",
-];
-
-export const implementation = (mapAAN) => (as) => [...as].sort(mapAAN);
-
 /**
  * ```
  * sort :: ((a, a) => n) => as => as
@@ -16,4 +7,5 @@ export const implementation = (mapAAN) => (as) => [...as].sort(mapAAN);
  * Sorts a list by applying the given sorting function.
  *
  */
-export const sort = sign(signatures, implementation);
+export const sort = <A>(compare: ((a1: A, a2: A) => number) | undefined) =>
+  (as: A[]) => [...as].sort(compare);
