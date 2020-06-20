5c11abb1bf112489bf670b42f1d37ec875f29be1
Matt McMahon <contact@matt-mcmahon.com>
2020-06-20 10:28:08 -0400

    Fix cap

--- source/cap/cap.ts
+++ source/cap/cap.ts
@@ -4,8 +4,7 @@
  * ```
  * @param {string} word word we want to capitalize
  */
-export function cap(word: string): string {
-  return typeof word === "string" && word.length > 0
+export const cap = (word: string): string =>
+  typeof word === "string" && word.length > 0
     ? word[0].toLocaleUpperCase() + word.substr(1)
-    : "";
-}
+    : word;
