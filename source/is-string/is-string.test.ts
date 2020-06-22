d0de364da0a59eaef8a7cb1f3c376854df36e5ae
Matt McMahon <contact@matt-mcmahon.com>
2020-06-22 17:29:28 -0400

    source/is-string.ts: Testing Fake Strings

--- source/is-string/is-string.test.ts
+++ source/is-string/is-string.test.ts
@@ -1,15 +1,15 @@
 import { describe } from "../../lib/describe.ts";
-import { isNumber } from "./is-number.ts";
+import { isString } from "./is-string.ts";
 
-describe("source/is-number", async ({ assert, inspect }) => {
+describe("source/is-string", async ({ assert, inspect }) => {
   const data: [unknown, boolean][] = [
-    [1, true],
-    [0, true],
-    [NaN, true],
-    [-Infinity, true],
-    [+Infinity, true],
+    [1, false],
+    [0, false],
+    [NaN, false],
+    [-Infinity, false],
+    [+Infinity, false],
     [undefined, false],
-    ["", false],
+    ["", true],
     [[], false],
     [false, false],
     [{}, false],
@@ -17,10 +17,28 @@ describe("source/is-number", async ({ assert, inspect }) => {
   ];
 
   const test = <A, B>([value, expected]: [A, B]) => {
-    const actual = isNumber(value);
-    const given = inspect`isNumber(${value})`;
+    const actual = isString(value);
+    const given = inspect`isString(${value})`;
     assert({ given, actual, expected });
   };
 
   data.forEach(test);
+});
+
+describe(`source/is-string.ts->string-like`, async (
+  { assert, inspect },
+) => {
+  const value = {
+    valueOf() {
+      return "fake v";
+    },
+    toString() {
+      return "fake s";
+    },
+  };
+  const actual = isString(value);
+  const expected = false;
+  const given = `valueOf() => "${value + `"; toString() => "${value}"`}`;
+  const should = `not be a string`;
+  assert({ actual, expected, value, given, should });
 });
