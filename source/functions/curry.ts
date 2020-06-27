a69d8446173d7b52cc47f991b1e29c8cd664f12b
Matt McMahon <contact@matt-mcmahon.com>
2020-06-27 15:15:34 -0400

    Converted curry/curryN to TS

--- source/functions/curry.ts
+++ source/functions/curry.ts
@@ -1,5 +1,4 @@
-import { Variadic, Curried } from "../types"
-
+import { curryN } from "../functions"
 /**
  * ```
  * curry :: ((a¹, a²..., aⁿ) => b) => a¹ => a²... => aⁿ => b
@@ -8,7 +7,7 @@ import { Variadic, Curried } from "../types"
  * Converts a _Variadic_ function into a series of _Unary_ functions that
  * produce the same final value.
  *
+ * @todo add support for Variadic Tuples in TypeScript 4
+ *
  */
-export declare function curry<A, B>(
-  originalFunction: Variadic<A, B>
-): Curried<A, B>
+export const curry = <F extends Function>(f: F) => curryN(f.length)(f)
