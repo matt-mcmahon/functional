9f4fdb87c3b59e54b8dcd33ff05bf79f4b1e3de8
Matt McMahon <contact@matt-mcmahon.com>
2020-06-27 15:23:37 -0400

    Converted partial to TS

---
+++ source/functions/partial.test.ts
@@ -0,0 +1,40 @@
+import { describe } from "@mwm/sign"
+import { partial } from "./partial"
+
+describe("partial", async ({ assert, inspect }) => {
+  const f = (a: string, b: string, c: string, d: string) => a + b + c + d
+
+  {
+    const given = f.toString()
+    const should = inspect`have a length of ${4}`
+    const actual = f.length
+    const expected = 4
+    assert({ given, should, actual, expected })
+  }
+
+  const [a, b, c, d] = ["a", "b", "c", "d"]
+  const f_AB = partial(f)(a, b)
+
+  const given = inspect`(${a}, ${b}, c, d) => ${a} + ${b} + c + d`
+
+  {
+    const expected = "function"
+    const actual = typeof f_AB
+    const should = inspect`be a ${expected}`
+    assert({ expected, actual, given, should })
+  }
+
+  {
+    const expected = "abcd"
+    const actual = f_AB(c, d)
+    assert({ expected, actual, given })
+  }
+
+  {
+    const f_ABC = partial(f_AB)(c)
+    const actual = f_ABC(d)
+    const expected = a + b + c + d
+    const given = inspect`two partial applications, ([${a}, ${b}]), ([${c}])`
+    assert({ expected, actual, given })
+  }
+})
---
+++ source/functions/partial.ts
@@ -0,0 +1,18 @@
+/**
+ * ```
+ * partial :: (a¹, a², …, aⁿ => b) => (a¹, …) => ... => (…, aⁿ) => b
+ * ```
+ * -----------------------------------------------------------------------------
+ *
+ * Creates a version of the supplied _n_-ary function that can be be partially
+ * applied.
+ *
+ * @todo add support for Variadic Tuples in TypeScript 4
+ */
+export const partial = <F extends Function>(f: F) =>
+  function g(...as: unknown[]) {
+    const signatures = [{ [`${f.name}${as.length} :: ...as => b`]: Infinity }]
+    return as.length < f.length
+      ? (...bs: unknown[]) => g(...as, ...bs)
+      : f(...as)
+  }
