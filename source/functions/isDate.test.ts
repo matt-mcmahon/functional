31b855b69e94b601ec376a5415dd54267901e58f
Matt McMahon <contact@matt-mcmahon.com>
2020-06-27 15:19:01 -0400

    New Function: isDate

---
+++ source/functions/isDate.test.ts
@@ -0,0 +1,23 @@
+import { describe } from "@mwm/sign"
+import { isDate } from "./isDate"
+
+describe("is-date", async ({ assert, inspect }) => {
+  const data: [unknown, boolean][] = [
+    [new Date(), true],
+    [undefined, false],
+    [NaN, false],
+    ["", false],
+    [[], false],
+    [false, false],
+    [{}, false],
+    [{ length: 0 }, false],
+  ]
+
+  const test = <A, B>([value, expected]: [A, B]) => {
+    const actual = isDate(value)
+    const given = inspect`isDate(${value})`
+    assert({ given, actual, expected })
+  }
+
+  data.forEach(test)
+})
