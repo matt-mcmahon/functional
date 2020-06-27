224aacc67fdfcc390c8e36204c55987906a8198d
Matt McMahon <contact@matt-mcmahon.com>
2020-06-27 15:18:35 -0400

    New Function: isArray

---
+++ source/functions/isArray.test.ts
@@ -0,0 +1,44 @@
+import { describe } from "@mwm/sign"
+import { isArray } from "./isArray"
+
+describe("is-array", async ({ assert, inspect }) => {
+  const data: [unknown, boolean][] = [
+    [null, false],
+    [undefined, false],
+    [NaN, false],
+    ["", false],
+    [[], true],
+    [false, false],
+    [{}, false],
+    [{ length: 0 }, false],
+  ]
+
+  const test = <A, B>([value, expected]: [A, B]) => {
+    const actual = isArray(value)
+    const given = inspect`isDefined(${value})`
+    assert({ given, actual, expected })
+  }
+
+  data.forEach(test)
+})
+
+// These tests won't compile unless isDefined has the  `: a is ...` type guard
+describe("is-array, type guard", async ({ assert, inspect }) => {
+  const f = (value: unknown) => (isArray(value) ? "array" : "error")
+
+  {
+    const expected = "array"
+    const value = [0, 1, 2]
+    const actual = f(value)
+    const should = "pass type guard"
+    assert({ actual, expected, value, should })
+  }
+
+  {
+    const expected = "error"
+    const value = "012"
+    const actual = f(value)
+    const should = "fail type guard"
+    assert({ actual, expected, value, should })
+  }
+})
