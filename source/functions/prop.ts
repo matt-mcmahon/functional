508e31a5ce8a04b0658fdc31b93855e78dd14137
Matt McMahon <contact@matt-mcmahon.com>
2020-06-27 15:25:06 -0400

    Converted prop to TS

---
+++ source/functions/prop.ts
@@ -0,0 +1,17 @@
+/**
+ * type `A` ensures that our object contains the property PropertyName
+ */
+type A<B, PropertyName extends string> = { [P in PropertyName]: B }
+
+/**
+ * ```
+ * prop :: k => a.k => b
+ * ```
+ * -----------------------------------------------------------------------------
+ *
+ * Returns the value of the given _property_ for the object.
+ *
+ */
+export const prop = <B, PropertyKey extends string>(
+  propertyKey: PropertyKey
+) => (a: A<B, PropertyKey>) => a[propertyKey]
