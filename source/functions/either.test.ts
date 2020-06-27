1b84407808cd39613e173438ff3af1b8dafec3dd
Matt McMahon <contact@matt-mcmahon.com>
2020-06-27 15:16:00 -0400

    Converted either to TS

---
+++ source/functions/either.test.ts
@@ -0,0 +1,59 @@
+import { describe } from "@mwm/sign"
+import { either } from "./either"
+import { T } from "../functions"
+
+describe("either", async ({ assert, inspect }) => {
+  const max = 10
+  const gt10 = (x: number) => x > max
+  const even = (x: number) => x % 2 === 0
+  const f = either(gt10)(even)
+
+  {
+    const value = 101
+    const expected = true
+    const actual = f(value)
+    const given = inspect`${value} > ${max}`
+    assert({ actual, expected, given })
+  }
+
+  {
+    const expected = true
+    const actual = f(8)
+    const given = inspect``
+    const should = inspect`true because 8 is even, got "${actual}"`
+    assert({ expected, actual, given, should })
+  }
+
+  {
+    const value = 8
+    const expected = true
+    const actual = f(value)
+    const given = inspect`${value} is even`
+    assert({ actual, expected, given })
+  }
+
+  {
+    const value = 7
+    const expected = false
+    const actual = f(value)
+    const given = inspect`${value} < ${max} and is odd`
+    assert({ actual, expected, given })
+  }
+
+  {
+    const throws = () => {
+      throw new Error("either should not execute me")
+    }
+    const plan = {
+      expected: true,
+      given: inspect`first predicate always returns true`,
+      should: inspect`not execute ${throws}`,
+    }
+    try {
+      const actual = either(T)(throws)("anything")
+      assert({ ...plan, actual })
+    } catch (err) {
+      assert({ ...plan, actual: false })
+    }
+  }
+})
--- source/functions/either.ts
+++ source/functions/either.ts
@@ -5,12 +5,10 @@
  * -----------------------------------------------------------------------------
  *
  * The __either__ combinator creates a _Unary_ from two functions. It returns
- * __b__ form calling `mapAB(a)` if __b__ is truthy. Otherwise it returns __c__
+ * __b__ from calling `mapAB(a)` if __b__ is truthy. Otherwise, it returns __c__
  * from calling `mapAC(a)`.
  *
  */
-export declare function either<A, B, C>(
-  mapAB: (a: A) => B,
-  mapAC: (a: A) => C,
+export const either = <A, B>(mapAB: (a: A) => B) => <C>(mapAC: (a: A) => C) => (
   a: A
-): B | C
+) => mapAB(a) || mapAC(a)
