080856cf85bf532866660ba24e168413d131577e
Matt McMahon <contact@matt-mcmahon.com>
2020-06-27 15:17:09 -0400

    Converted head to TS

--- source/head/head.js
+++
@@ -1,7 +0,0 @@
-import { sign } from "@mwm/sign"
-
-export const signatures = [{ "head :: as => a": 1 }]
-
-export const implementation = as => as[0]
-
-export const head = sign(signatures, implementation)
