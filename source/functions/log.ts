d5748b05159a2fe192139a2e8af6351412f86d4c
Matt McMahon <contact@matt-mcmahon.com>
2020-06-27 15:22:40 -0400

    New Function: log

---
+++ source/functions/log.ts
@@ -0,0 +1,14 @@
+/**
+ * ```
+ * log :: s -> a -> a
+ * ```
+ * -----------------------------------------------------------------------------
+ * Takes a string, a _value_, logs the string and the value, and then returns
+ * the _value_.
+ */
+export const log = (s: string) => <A>(a: A) => {
+  console.groupCollapsed(s)
+  console.log(a)
+  console.groupEnd()
+  return a
+}
