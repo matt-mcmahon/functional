94890b66469cdc10b4176f3e61602558a78f1769
Matt McMahon <contact@matt-mcmahon.com>
2020-06-27 15:16:13 -0400

    Converted equals to TS

--- source/equals/index.js
+++
@@ -1,1 +0,0 @@
-export { equals } from "./equals"
---
+++ source/functions/equals.test.ts
@@ -0,0 +1,21 @@
+import { describe } from "@mwm/sign"
+import { equals } from "./equals"
+
+describe("equals", async ({ assert, inspect }) => {
+  const values: [unknown, unknown, boolean][] = [
+    ["a", "b", false],
+    ["a", "a", true],
+    [1, 1, true],
+    [1, 2, false],
+    [NaN, NaN, false],
+    [undefined, undefined, true],
+    [undefined, null, false],
+    [null, null, true],
+  ]
+
+  values.forEach(([a, b, expected]) => {
+    const actual = equals(a)(b)
+    const given = inspect`equals(${a}, ${b})`
+    assert({ expected, actual, given })
+  })
+})
