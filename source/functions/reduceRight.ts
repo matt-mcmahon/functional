68e40c2d50458dc724d82f68acccf85dd4d6ff83
Matt McMahon <contact@matt-mcmahon.com>
2020-06-27 15:25:40 -0400

    Converted reduceRight to TS

--- source/functions/reduceRight.ts
+++ source/functions/reduceRight.ts
@@ -1,20 +1,16 @@
-import { Reducer, Iterable } from "../types"
-
 /**
  * ```
  * reduceRight :: ((a, b) => a) => a => [bⁿ, ..., b², b¹, b⁰] => a
  * ```
  * -----------------------------------------------------------------------------
  *
- * __reduceRight__ is a _Unary_ _Combinator_, that reduces an _Iterable_, in
+ * __reduceRight__ is a _Unary_ _Combinator_, that reduces an _Array_, in
  * last-to-first order, to a single value. It does this by passing an initial
- * value, the _accumulator_ and the last value of the _Iterable_, to _reducer_
+ * value, the _accumulator_ and the last value of the _Array_, to _reducer_
  * function. The return value for that call is used as the accumulator for the
  * next iteration.
  *
  */
-export declare function reduceRight<A, I, V>(
-  reducer: Reducer<A, I, V>,
-  accumulator: A,
-  iterable: Iterable<I>
-): V
+export const reduceRight = <A, B>(reducer: (a: A, b: B) => A) => (a: A) => (
+  bs: B[]
+) => (bs.length > 0 ? bs.reduceRight(reducer, a) : a)
