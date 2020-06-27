55b46c6c00298085ddcf985eecb1c9e98716f7ac
Matt McMahon <contact@matt-mcmahon.com>
2020-06-27 15:19:18 -0400

    Converted isDefined to TS

--- source/isDefined/isDefined.js
+++
@@ -1,7 +0,0 @@
-import { sign } from "@mwm/sign"
-
-export const signatures = ["isDefined :: a => boolean"]
-
-export const implementation = a => a === a && a !== undefined && a !== null
-
-export const isDefined = sign(signatures, implementation)
