98c0deaa34878ab5ccad77944348e5b861902cb9
Matt McMahon <contact@matt-mcmahon.com>
2020-06-27 20:32:25 -0400

    Removed test From Dist

--- dist/functions/compose.d.ts
+++ dist/functions/compose.d.ts
@@ -1,3 +1,7 @@
-import { Compose } from "../types";
+export interface Compose<B, A> {
+    (b: B): A;
+    from: <C>(f: (c: C) => B) => Compose<C, A>;
+    call(a: A): B;
+}
 declare const last: <B, A>(f: (b: B) => A) => Compose<B, A>;
 export { last as compose };
--- dist/functions/pipe.d.ts
+++ dist/functions/pipe.d.ts
@@ -1,3 +1,7 @@
-import { Pipe } from "../types";
+export interface Pipe<A, B> {
+    (a: A): B;
+    then: <C>(f: (b: B) => C) => Pipe<A, C>;
+    call(a: A): B;
+}
 declare const first: <A, B>(f: (a: A) => B) => Pipe<A, B>;
 export { first as pipe };
