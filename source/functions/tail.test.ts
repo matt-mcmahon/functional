09fe104f0da3c50f0b131dbc40de454875e747ae
Matt McMahon <contact@matt-mcmahon.com>
2020-08-20 18:31:20 -0400

    TS4: tail

--- source/functions/tail.test.ts
+++ source/functions/tail.test.ts
@@ -1,40 +1,41 @@
-import { describe } from "@mwm/sign"
-import { tail } from "./tail"
+import { describe } from "@mwm/sign";
+import { tail } from "./tail";
 
 describe("tail", async ({ assert, inspect }) => {
   {
-    const arg = ["a", "b", "c"]
-    const expected = ["b", "c"]
-    const actual = tail(arg)
-    const given = inspect`tail(${arg})`
-    const should = inspect`${expected}`
-    assert({ actual, expected, given, should })
+    const arg = ["a", "b", "c"] as const;
+    const expected = ["b", "c"] as const;
+    const actual = tail(arg);
+    const given = inspect`tail(${arg})`;
+    const should = inspect`${expected}`;
+    assert({ actual, expected, given, should });
   }
 
   {
-    const arg = ["a"]
-    const expected: string[] = []
-    const actual = tail(arg)
-    const given = inspect`tail(${arg})`
-    const should = inspect`${expected}`
-    assert({ actual, expected, given, should })
+    const arg = ["a"] as const;
+    const expected = [] as const;
+    const actual = tail(arg);
+    const given = inspect`tail(${arg})`;
+    const should = inspect`${expected}`;
+    assert({ actual, expected, given, should });
   }
 
   {
-    const arg: string[] = []
-    const expected: string[] = []
-    const actual = tail(arg)
-    const given = inspect`tail(${arg})`
-    const should = inspect`${expected}`
-    assert({ actual, expected, given, should })
+    const arg = [] as const;
+    const expected = [] as const;
+    //@ts-ignore
+    const actual = tail(arg);
+    const given = inspect`tail(${arg})`;
+    const should = inspect`${expected}`;
+    assert({ actual, expected, given, should });
   }
 
   {
-    const arg: string[][] = [["a", "b", "c"]]
-    const expected: string[] = []
-    const actual = tail(arg)
-    const given = inspect`tail(${arg})`
-    const should = inspect`${expected}`
-    assert({ actual, expected, given, should })
+    const arg = [["a", "b", "c"]] as const;
+    const expected = [] as const;
+    const actual = tail(arg);
+    const given = inspect`tail(${arg})`;
+    const should = inspect`${expected}`;
+    assert({ actual, expected, given, should });
   }
-})
+});
--- source/functions/tail.ts
+++ source/functions/tail.ts
@@ -7,4 +7,6 @@
  * Returns the portion of an array not including the first element.
  *
  */
-export const tail = <A>(as: A[]) => as.slice(1)
+export const tail = <Ignored extends unknown, AS extends readonly unknown[]>(
+  [ignored, ...as]: readonly [Ignored, ...AS],
+): AS => as;
