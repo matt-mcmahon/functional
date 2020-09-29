551bcd8aec1bf7a77e1174538877049f5c5366d4
Matt McMahon <contact@matt-mcmahon.com>
2020-09-29 19:11:28 -0400

    Add Explicit Return Type: always

--- source/functions/always.ts
+++ source/functions/always.ts
@@ -6,4 +6,5 @@
  *
  * Creates a function that always returns _a_, ignoring any arguments.
  */
-export const always = <A>(a: A) => (...bs: unknown[]) => a
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const always = <A>(a: A) => (...bs: unknown[]): A => a
