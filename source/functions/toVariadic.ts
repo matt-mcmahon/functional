333fe74f6de5fb008405fbd92d599278debe94ae
Matt McMahon <contact@matt-mcmahon.com>
2020-09-29 23:14:55 -0400

    Fix Return Type, JSDoc: toVariadic

--- source/functions/toVariadic.ts
+++ source/functions/toVariadic.ts
@@ -4,10 +4,10 @@
  * ```
  * -----------------------------------------------------------------------------
  *
- * Takes a _Unary_ function that accepts an array as its argument, and returns
- * that accepts any number of arguments instead.
+ * Takes a _Unary_ function that accepts an array as its only argument, and
+ * returns a function that accepts any number of arguments instead.
  *
  */
 export const toVariadic = <AS extends unknown[], B>(u: (as: AS) => B) => (
   ...as: AS
-) => u(as)
+): B => u(as)
