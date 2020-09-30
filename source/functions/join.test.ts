497f970b0d66be0051ee94a950a87dca44626b74
Matt McMahon <contact@matt-mcmahon.com>
2020-09-29 21:04:18 -0400

    Refactor join
    - improper use of generics
    - the join character should be either a string or undefined
    - the array can be of any type or types and will be converted into
      strings before concatenating.
    - test undefined separator, single element array, irregular array

--- source/functions/join.test.ts
+++ source/functions/join.test.ts
@@ -9,7 +9,46 @@ describe("join", async ({ assert, inspect }) => {
     const actual = f(list)
     const expected = "a-b-c"
     const given = inspect`join(${char})(${list})`
-    const should = inspect`be ${expected}`
-    assert({ given, should, actual, expected })
+    assert({ given, actual, expected })
+  }
+
+  {
+    const char = undefined
+    const f = join(char)
+    const list = ["a", "b", "c"]
+    const actual = f(list)
+    const expected = "a,b,c"
+    const given = inspect`undefined separator, ${list}`
+    assert({ given, actual, expected })
+  }
+
+  {
+    const char = "-"
+    const f = join(char)
+    const list = ["a"]
+    const actual = f(list)
+    const expected = "a"
+    const given = inspect`single element, ${list}`
+    assert({ given, actual, expected })
+  }
+
+  {
+    const char = ", "
+    const f = join(char)
+    const list = [
+      "a",
+      {
+        toString() {
+          return "object!"
+        },
+      },
+      "b",
+      1,
+      "c",
+    ]
+    const actual = f(list)
+    const expected = "a, object!, b, 1, c"
+    const given = inspect`irregular array`
+    assert({ given, actual, expected })
   }
 })
--- source/functions/join.ts
+++ source/functions/join.ts
@@ -1,14 +1,21 @@
-interface Joinable<A> {
-  join: (separator?: A | undefined) => A
-}
-
 /**
  * ```
- * join:: a => as => a
+ * join:: a => bs => a
  * ```
  * -----------------------------------------------------------------------------
  *
- * Takes a joining value, a list with the a `join(a:A):B` method, and returns B
+ * __join__ takes an optional separator string, __a__, an array, __bs__ of any
+ * type and returns a new string by concatenating all of the elements in the
+ * array, converting them to strings if necessary, and separating each element
+ * by either a comma, `","`, or __a__.
  *
+ * If the array has only one item, then that item will be returned without using
+ * the separator.
+ *
+ * ```
+ * [1, 2, 3].join("---") <=> join("---")([1, 2, 3]) <=> "1---2---3"
+ * [1, 2, 3].join()      <=> join()([1, 2, 3])      <=> "1,2,3"
+ * ```
  */
-export const join = <A>(a: A) => (as: Joinable<A>) => as.join(a)
+export const join = (a: string | undefined) => <A>(bs: A[]): string =>
+  bs.join(a)
