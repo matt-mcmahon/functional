e5da6736a69219f07ec27c816c518b791a56efe7
Matt McMahon <contact@matt-mcmahon.com>
2020-09-29 20:31:52 -0400

    Better test messages: isFunction.test.ts

--- source/functions/isFunction.test.ts
+++ source/functions/isFunction.test.ts
@@ -5,33 +5,41 @@ describe("isFunction", async ({ assert, inspect }) => {
   const expected = true
   const should = inspect`be a function`
   {
-    const actual = isFunction(function test() {})
-    const given = inspect`function test() {}`
+    const actual = isFunction(function test() {
+      "do nothing"
+    })
+    const given = inspect`normal function`
     assert({ actual, expected, given, should })
   }
 
   {
-    const actual = isFunction(function* test() {})
-    const given = inspect`function* test() {}`
+    const actual = isFunction(function* test() {
+      yield
+    })
+    const given = inspect`generator function`
     assert({ actual, expected, given, should })
   }
 
   {
-    const actual = isFunction(async function test() {})
-    const given = inspect`async function test() {}`
+    const actual = isFunction(async function test() {
+      "do nothing"
+    })
+    const given = inspect`async function`
     assert({ actual, expected, given, should })
   }
 
   {
-    const actual = isFunction(() => {})
-    const given = inspect`() => {}`
+    const actual = isFunction(() => {
+      "do nothing"
+    })
+    const given = inspect`arrow function`
     assert({ actual, expected, given, should })
   }
 
   {
     const expected = false
-    const actual = isFunction("null")
-    const given = inspect`${null}`
+    const actual = isFunction(null)
+    const given = inspect`the value ${null}`
     const should = inspect`not be a function`
     assert({ actual, expected, given, should })
   }
@@ -39,7 +47,7 @@ describe("isFunction", async ({ assert, inspect }) => {
   {
     const expected = false
     const actual = isFunction("function")
-    const given = inspect`${"function"}`
+    const given = inspect`the string ${"function"}`
     const should = inspect`not be a function`
     assert({ actual, expected, given, should })
   }
