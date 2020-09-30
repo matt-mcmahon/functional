685b80d6a3b9da53fbe9d93c884b8eed13e90d79
Matt McMahon <contact@matt-mcmahon.com>
2020-09-29 22:35:12 -0400

    Edit split
    - accept more array-like types
    - add explicit return type
    - respect split limit
    - update JS Doc
    - test JS Doc examples

--- source/functions/split.test.ts
+++ source/functions/split.test.ts
@@ -5,10 +5,39 @@ describe("split", async ({ assert, inspect }) => {
   {
     const value = "one two three"
     const char = " "
-    const expected = value.split(char)
-    const actual = split(char)(value)
-    const given = inspect`split(${char})(${value})`
-    assert({ value, expected, actual, given })
+    {
+      const expected = value.split(char)
+      const actual = split(char)(value)
+      const given = inspect`split(${char})(${value})`
+      assert({ value, expected, actual, given })
+    }
+    {
+      const expected = ["one", "two", "three"]
+      const actual = split(char)(value)
+      const given = inspect`split(${char})(${value})`
+      assert({ value, expected, actual, given })
+    }
+    {
+      const limit = 2
+      const actual = split(char, limit)(value)
+      const expected = ["one", "two"]
+      const given = inspect`split(${char}, ${limit})(${value})`
+      assert({ value, expected, actual, given })
+    }
+    {
+      const limit = 9
+      const actual = split(char, limit)(value)
+      const expected = ["one", "two", "three"]
+      const given = inspect`split(${char}, ${limit})(${value})`
+      assert({ value, expected, actual, given })
+    }
+    {
+      const limit = 0
+      const actual = split(char, limit)(value).length
+      const expected = 0
+      const given = inspect`split(${char}, ${limit})(${value})`
+      assert({ value, expected, actual, given })
+    }
   }
 
   {
@@ -70,7 +99,8 @@ describe("split", async ({ assert, inspect }) => {
     const value = "𝟘𝟙𝟚𝟛"
     const char = ""
     const expected: string[] = ["𝟘", "𝟙", "𝟚", "𝟛"]
+    const given = inspect`surrogate pairs, ${value}`
     const actual = split(char)(value)
-    assert({ value, expected, actual })
+    assert({ value, expected, actual, given })
   }
 })
