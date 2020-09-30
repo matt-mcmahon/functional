c5b7b7ea917e1f4622e911372a7b1924ccffcffc
Matt McMahon <contact@matt-mcmahon.com>
2020-09-29 21:08:43 -0400

    Add Explicit Return Type: log

--- source/functions/log.ts
+++ source/functions/log.ts
@@ -1,3 +1,4 @@
+/* global console */
 /**
  * ```
  * log :: s -> a -> a
@@ -6,7 +7,7 @@
  * Takes a string, a _value_, logs the string and the value, and then returns
  * the _value_.
  */
-export const log = (s: string) => <A>(a: A) => {
+export const log = (s: string) => <A>(a: A): A => {
   console.groupCollapsed(s)
   console.log(a)
   console.groupEnd()
