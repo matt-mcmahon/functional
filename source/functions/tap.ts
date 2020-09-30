dc0df8a6e3570f08d0607739123c85c8a781991d
Matt McMahon <contact@matt-mcmahon.com>
2020-09-29 23:00:09 -0400

    Add Explicit Return Type: tap

--- source/functions/tap.ts
+++ source/functions/tap.ts
@@ -7,7 +7,7 @@
  * _sideEffect_. Ignores any value returned by the side effect function, and
  * returns the _value_ instead.
  */
-export const tap = <A>(f: (a: A) => void) => (a: A) => {
+export const tap = <A>(f: (a: A) => void) => (a: A): A => {
   f(a)
   return a
 }
