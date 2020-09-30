3bf20e4c3d8a816a759377ea8e69695551c2ff29
Matt McMahon <contact@matt-mcmahon.com>
2020-09-29 21:35:54 -0400

    Add JS Doc, Explicit Return Type: reverse

--- source/functions/reverse.ts
+++ source/functions/reverse.ts
@@ -1,1 +1,14 @@
-export const reverse = <A>(as: ArrayLike<A>) => Array.from(as).reverse()
+/**
+ * ```
+ * reverse :: as => as
+ * ```
+ * -----------------------------------------------------------------------------
+ *
+ * Returns an array created from __`as`__, sorted in reverse order.
+ *
+ * ```
+ * const as = [1, 2, 3]
+ * Array.from(as).reverse() <=> reverse(as) <=> [3, 2, 1]
+ * ```
+ */
+export const reverse = <A>(as: ArrayLike<A>): A[] => Array.from(as).reverse()
