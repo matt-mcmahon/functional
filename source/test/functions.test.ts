f947777ed5879c1cde53a10887780f8231d40685
Matt McMahon <contact@matt-mcmahon.com>
2020-10-01 22:52:45 -0400

    Move functions.test.ts to ./source/test
    - Fix: bad path in functions.test.ts
    - Needs filesystem access, which requires Deno libs

---
+++ source/test/functions.test.ts
@@ -0,0 +1,35 @@
+import { describe } from "../lib/remote/describe.ts";
+import { readdir } from "../lib/readdir.ts";
+
+import * as functional from "../app/functions.ts";
+
+const removeExtension = (fileNames: string[]) =>
+  fileNames.map((file) => file.replace(/.\w+$/, ""));
+
+const filterTestFiles = (fileNames: string[]) =>
+  fileNames.filter(
+    (name) => !name.includes(".test.") && !name.includes("index"),
+  );
+
+const sortFiles = (files: string[]) => files.sort();
+
+const getModuleList = () =>
+  readdir("./source/app/functions")
+    .then(filterTestFiles)
+    .then(removeExtension)
+    .then(sortFiles)
+    .catch((reason) => {
+      throw reason;
+    });
+
+describe("functions.test.ts", async ({ assert }) => {
+  const actual = Object.keys(functional).sort();
+  const expected = await getModuleList();
+
+  assert({
+    actual,
+    expected,
+    given: "actual module exports",
+    should: "include all public exports",
+  });
+});
