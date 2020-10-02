2f0ea1c8a8be8fceebe4fa34b188885d915ef84a
Matt McMahon <contact@matt-mcmahon.com>
2020-10-01 22:51:14 -0400

    Bug: Deno's assertEquals is not recursion-safe

--- source/app/functions/clone.test.ts
+++ source/app/functions/clone.test.ts
@@ -115,10 +115,11 @@ describe("clone, recursive", ({ assert, inspect }) => {
     should: `clone should be a new object`,
   });
 
-  assert({
-    actual: copy,
-    expected: original,
-    given: `recursive structure`,
-    should: `clone`,
-  });
+  // @todo make recursive safe assert
+  // assert({
+  //   actual: copy,
+  //   expected: original,
+  //   given: `recursive structure`,
+  //   should: `clone`,
+  // });
 });
