b0eb557c0505e29a6ce840aeede225ba460f1b8c
Matt McMahon <matt@matthewcodes.com>
2022-05-01 15:26:39 -0400

    Add ./scripts/build-npm.ts
    - ignore npm/* build folder

---
+++ scripts/build-npm.ts
@@ -0,0 +1,21 @@
+import { build, emptyDir } from "https://deno.land/x/dnt@0.23.0/mod.ts";
+import { getLatestVersion } from "https://deno.land/x/bump_version@v0.2.1/bump.ts";
+import pack from "./package.json" assert { type: "json" };
+
+await emptyDir("./npm");
+
+await build({
+  entryPoints: [{
+    name: ".",
+    path: "./functions.ts",
+  }],
+  outDir: "./npm",
+  shims: { deno: false },
+  package: { ...pack, ...{ version: await getLatestVersion() } },
+  typeCheck: false,
+  test: false,
+});
+
+// post build steps
+Deno.copyFileSync("LICENSE", "npm/LICENSE");
+Deno.copyFileSync("README.md", "npm/README.md");
