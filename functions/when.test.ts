a1b8c9c92818a4fce0f31fb90d793190c8b3fa6f
Matt McMahon <matt@matthewcodes.com>
2022-05-30 19:43:03 -0400

    New functions/when
    - complement to unless

---
+++ functions/when.test.ts
@@ -0,0 +1,32 @@
+import { assertEquals } from "https://deno.land/std@0.136.0/testing/asserts.ts";
+import { when } from "./when.ts";
+
+Deno.test("when", () => {
+  {
+    const isA = (v: unknown): v is "A" => v === "A";
+
+    const makeB = (_: "A"): "B" => "B";
+
+    const when_isA_makeB = when(isA)(makeB);
+
+    assertEquals(when_isA_makeB("C"), "C");
+
+    assertEquals(when_isA_makeB("A"), "B");
+  }
+
+  {
+    const max = Number.MAX_SAFE_INTEGER;
+    const min = Number.MIN_SAFE_INTEGER;
+
+    const inc = (n: number) => n + 1;
+    const canIncrement = (n: number): n is number => n < max && n >= min;
+    const whenCanIncrement = when(canIncrement);
+    const saferInc = whenCanIncrement(inc);
+
+    assertEquals(saferInc(max), max);
+    assertEquals(saferInc(max - 1), max);
+    assertEquals(saferInc(min), min + 1);
+
+    assertEquals(saferInc(1), 2);
+  }
+});
---
+++ functions/when.ts
@@ -0,0 +1,14 @@
+/**
+ * ```haskell
+ * unless :: (a => boolean) => (a => b) => a => a|b
+ * ```
+ * -----------------------------------------------------------------------------
+ *
+ * Return `a => b` __unless__ the `a => boolean` is `true`, in that case return
+ * `a`.
+ */
+export const when =
+  <X, A extends X>(xIsA: (x: X) => x is A) =>
+  <B>(mapAB: (a: A) => B) =>
+  (x: X): B | X =>
+    xIsA(x) ? mapAB(x) : x;
