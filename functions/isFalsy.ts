35fb98ee56836c86c826ee3686e28400daeae9be
Matt McMahon <matt@matthewcodes.com>
2023-05-15 14:46:13 -0400

    Fix functions/isFalsy
    - acts as type guard for many falsy type literals

--- functions/isFalsy.ts
+++ functions/isFalsy.ts
@@ -1,9 +1,19 @@
+export type Falsy = null | undefined | false | 0 | -0 | 0n  | ""
+
 /**
  * ```haskell
  * isFalsy :: a => boolean
  * ```
  * -----------------------------------------------------------------------------
  *
- * Returns `true` if __a__ is a falsy value, false otherwise.
+ * Returns `true` if __a__ is a falsy value, `false` otherwise. Falsy values 
+ * include: `null`, `undefined`, `false`, `NaN`, `0`, `-0`, `0n`, and `""`. 
+ * 
+ * When isFalsy is used as [type guard][1], it includes several 
+ * [literal typess][2]. The values `-0` and `NaN` are not a valid literal types 
+ * and cannot be included in the type predicate.
+ * 
+ * [1]: https://www.typescriptlang.org/docs/handbook/2/narrowing.html
+ * [2]: https://www.typescriptlang.org/docs/handbook/2/everyday-types.html#literal-types
  */
-export const isFalsy = (a: unknown): boolean => !a;
+export const isFalsy = (a: unknown): a is Falsy => !a;
