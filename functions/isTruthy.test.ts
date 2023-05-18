dca49ac272adc0f03604073064a39a1f24cc9029
Matt McMahon <matt@matthewcodes.com>
2023-05-18 11:27:09 -0400

    Fixup funcitons/isTruthy

--- functions/isTruthy.test.ts
+++ functions/isTruthy.test.ts
@@ -1,4 +1,8 @@
+// deno-lint-ignore-file ban-ts-comment
+
+import { assert } from "npm:spec.ts";
 import { assertEquals } from "testing";
+import { Falsy } from "./isFalsy.ts";
 import { isTruthy } from "./isTruthy.ts";
 
 Deno.test("isTruthy", () => {
@@ -26,7 +30,45 @@ Deno.test("isTruthy", () => {
     [NaN, false],
   ];
 
-  for (const [actual, expected] of data) {
-    assertEquals(isTruthy(actual), expected);
+  data.forEach(([actual, expected], index) => {
+    assertEquals(
+      isTruthy(actual),
+      expected,
+      `[${index}]: ${actual} ${expected ? "should" : "should not"} be truthy`,
+    );
+  });
+
+  // Type Guard Tests, raise compiler errors:
+  {
+    const never = {} as never;
+    const data: Falsy[] = [
+      "",
+      0,
+      false,
+      0,
+      0n,
+      NaN as Falsy,
+    ];
+    data.forEach((a) => {
+      if (isTruthy(a)) assert(a, never);
+    });
+
+    {
+      const a = "something";
+      //@ts-expect-error
+      if (isTruthy(a)) assert(a, never);
+    }
+
+    {
+      const a = 5;
+      //@ts-expect-error
+      if (isTruthy(a)) assert(a, never);
+    }
+
+    {
+      const a = { foo: 5 };
+      //@ts-expect-error
+      if (isTruthy(a)) assert(a, never);
+    }
   }
 });
